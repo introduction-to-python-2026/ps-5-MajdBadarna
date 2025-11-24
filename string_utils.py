94e4d68fc2c5421e1afa034ad9efc6afdfb05d86
MajdBadarna <q7ztqqwrn2@privaterelay.appleid.com>
2025-11-24 19:31:08 +0200

    Add functions to split formulas by case and digit
    Implemented functions to split a formula before uppercase letters and at the first digit.

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,39 @@
+def split_before_each_uppercases(formula):
+    if formula == "":
+        return []
+
+    start = 0
+    end = 1
+    split_formula = []
+
+    for char in formula[1:]:
+        if char.isupper():
+            split_formula.append(formula[start:end])
+            start = end
+        end += 1
+
+    split_formula.append(formula[start:end])
+
+    return split_formula
 
 
 
-def split_before_uppercases(formula):
-    pass  # replace the pass with your code
 
-def split_at_digit(formula):
-    pass  # replace the pass with your code
+def split_at_first_digit(formula):
+    digit_location = 1
+
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        prefix = formula[:digit_location]
+        number = int(formula[digit_location:])
+        return prefix, number
+
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.
